4b1d9da91b8e33b9059778617dc08cb825db0619
jeeeeeenno <139312392+jeeeeeenno@users.noreply.github.com>
2023-09-08 06:55:17 +0900

    Update DetailView.dart

--- DetailView.dart
+++ DetailView.dart
@@ -13,17 +13,21 @@ class DetailView extends StatelessWidget {
       child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-           HeaderWidget(),
+           HeaderWidget(), //Header 로고 
+           ProfileWidget(), //Profile 로고
             ElevatedButton(
               onPressed: () {
                 Get.find<DetailController>().goToMain();
               },
+           //   child: TextList(),
               child: Text('goToMain'),
             ),
             ElevatedButton(
               onPressed: () {
                 controller.work("test성공");
               },
+            
+            
               child: Obx(
                 () => Text('${controller.mainDB.value.detailTest}'),
               ),
@@ -52,3 +56,50 @@ class HeaderWidget extends StatelessWidget {
     );
   }
 }
+
+
+
+
+//Profile png
+class ProfileWidget extends StatelessWidget {
+  const ProfileWidget({Key? key});
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: Container(
+        child: Image.asset('assets/images/profile-images.png'),
+      ),
+    );
+  }
+}
+
+//TextList
+class TextList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
+      children: <Widget>[
+        Text(
+          'Detail',
+          style: TextStyle(fontSize: 18),
+        ),
+        Text(
+          '>절약금액',
+          style: TextStyle(fontSize: 18),
+        ),
+        Text(
+          '약 500,000원',
+          style: TextStyle(fontSize: 18),
+        ),
+        Text(
+          '추산치',
+          style: TextStyle(fontSize: 18),
+        ),
+      ],
+    );
+  }
+}
+
